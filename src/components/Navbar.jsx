337ab852e18f7143e5d4cb43cab3df18be116004
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-03-29 21:33:00 +0700

    fixing bug navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,20 +11,20 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">Al-Kareem</h1>
-      <ul className="md:flex hidden">
+      <ul className="hidden md:flex ">
         <li className="p-4">Home</li>
         <li className="p-4">Explore</li>
         <li className="p-4">Profile</li>
         <li className="p-4">About</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {!nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
       </div>
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+            ? "fixed left-[-100%]" 
+            : "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
         }
       >
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
