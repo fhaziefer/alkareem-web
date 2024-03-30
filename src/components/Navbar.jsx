7f4420e7929c2e77e9fe50dd4c5d58f2c25c48cd
Yusuf Fadlulloh <fhaziefer99@gmail.com>
2024-03-31 02:43:05 +0700

    setting hidden admin page just for admin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [isLogin, setLogin] = useState(false);
+  const isAdmin = useRef(false);
   const navigate = useNavigate();
+  
+  const userData = JSON.parse(window.localStorage.getItem("LOGIN_DATA"));
+  const role = userData.role;
+  if (role === "USER") {
+    isAdmin.current = false;
+  } else {
+    isAdmin.current = true;
+  }
 
   const handleLogin = () => {
     setLogin(!isLogin);
@@ -23,14 +32,22 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]"><Link to="/">Al-Kareem</Link></h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+        <Link to="/">Al-Kareem</Link>
+      </h1>
       <ul className="hidden md:flex ">
         <li className="p-4">
           <Link to="/">Home</Link>
         </li>
-        <li className="p-4">
-          <Link to="/explore">Explore</Link>
-        </li>
+        {isAdmin.current ? (
+          <li className="p-4">
+            <Link to="/admin">Admin</Link>
+          </li>
+        ) : (
+          <li className="p-4">
+            <Link to="/explore">Explore</Link>
+          </li>
+        )}
         <li className="p-4">
           <Link to="/profile">Profile</Link>
         </li>
@@ -52,15 +69,21 @@ const Navbar = () => {
         }
       >
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
-        <Link to="/">Al-Kareem</Link>
+          <Link to="/">Al-Kareem</Link>
         </h1>
         <ul className="upercase p-4">
           <li className="p-4 border-b border-gray-600">
             <Link to="/">Home</Link>
           </li>
-          <li className="p-4 border-b border-gray-600">
-            <Link to="/explore">Explore</Link>
-          </li>
+          {isAdmin.current ? (
+            <li className="p-4 border-b border-gray-600">
+              <Link to="/admin">Admin</Link>
+            </li>
+          ) : (
+            <li className="p-4 border-b border-gray-600">
+              <Link to="/admin">Explore</Link>
+            </li>
+          )}
           <li className="p-4">Search</li>
         </ul>
         <ul className="w-full upercase p-4 absolute bottom-0">
